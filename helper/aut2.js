786d58ce6133ba6c998539415fa229597f4a56d4
ahmed elbakly <ahmedelbakly258@gmail.com>
2023-10-09 09:56:24 +0300

    commit2

--- helper/aut2.js
+++ helper/aut2.js
@@ -1,23 +1,23 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
-export const authReq = async (req, res, next) => {
-  const shafrat = process.env.SHFARAT.split(" ");
+// import dotenv from "dotenv";
+// dotenv.config({ path: "../.env" });
+// export const authReq = async (req, res, next) => {
+//   const shafrat = process.env.SHFARAT.split(" ");
 
-  const save = await req.headers.save;
+//   const save = await req.headers.save;
 
-  try {
-    if (!save) {
-      return res.json("communicate with backend developer");
-    } else {
-      const pass = shafrat.includes(save);
-      if (!pass) {
-        return res.json("communicate with backend developer ");
-      } else {
-        // console.log(pass);
-        next();
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
+//   try {
+//     if (!save) {
+//       return res.json("communicate with backend developer");
+//     } else {
+//       const pass = shafrat.includes(save);
+//       if (!pass) {
+//         return res.json("communicate with backend developer ");
+//       } else {
+//         // console.log(pass);
+//         next();
+//       }
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
