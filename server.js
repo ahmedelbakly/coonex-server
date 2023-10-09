2197a5e2b7dee806135043fe0dd0c55a0133fbc5
ahmed elbakly <ahmedelbakly258@gmail.com>
2023-10-09 15:10:12 +0300

    one

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import connectDB from "./db.mjs"
 import favicon from 'serve-favicon'
-import userRouter from "././routes/productRoute.mjs"
+import userRouter from "./routes/productRoute.mjs"
 import productRouter from "./routes/productRoute.mjs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
